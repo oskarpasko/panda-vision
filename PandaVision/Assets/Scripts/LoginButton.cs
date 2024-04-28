39af4d0818af7e31f6270efe20bb0b67878faf21
Oskar <oskarpasko@gmail.com>
2024-04-28 14:00:59 +0200

    dodanie testowego skryptu testującego przycisk

---
+++ PandaVision/Assets/Scripts/LoginButton.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class LoginButton : MonoBehaviour {
+	public Button yourButton;
+
+	void Start () {
+		Button btn = yourButton.GetComponent<Button>();
+		btn.onClick.AddListener(TaskOnClick);
+	}
+
+	void TaskOnClick(){
+		Debug.Log ("Zalogowano!");
+	}
+}
