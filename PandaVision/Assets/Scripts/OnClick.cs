4dd1c9749ce08e0e28a18c0d0bbec6f32d02c4ae
Oskar <oskarpasko@gmail.com>
2024-05-17 00:14:38 +0200

    przygotowa nie podstawowego "klikającego" canvasa

---
+++ PandaVision/Assets/Scripts/OnClick.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class OnClick : MonoBehaviour {
+	public Button loginButton;
+
+	void Start () {
+
+	}
+
+	void Click(){
+		Debug.Log ("You have clicked the button!");
+	}
+
+	void SendMessage() {
+		Debug.Log ("You have clicked the button!");
+	}
+}
