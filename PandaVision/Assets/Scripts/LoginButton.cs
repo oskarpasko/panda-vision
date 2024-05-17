abce562a836b2eeb9f8990c4a01e513e4c4aa946
Oskar <oskarpasko@gmail.com>
2024-05-17 13:54:18 +0200

    logowanie sztywnymi danymi
    zalogowanie się sztywnymi danymi i przejście do nowej sceny

--- PandaVision/Assets/Scripts/LoginButton.cs
+++ PandaVision/Assets/Scripts/LoginButton.cs
@@ -7,13 +7,17 @@ public class LoginButton : MonoBehaviour {
 	
 	/// <param name="loginButton"> Przechowujemy obiekt naszego przycisku </param>
 	public Button loginButton;
+	public InputField emailField;
+	public InputField passwordField;
 	void Start () {
 		loginButton.onClick.AddListener(LoginButtonClicked);
 	}
 
 	void LoginButtonClicked (){
 
-		//przejście do nastepnej sceny
-		SceneManager.LoadScene("TestScene");
+		if(emailField.text == "admin" && passwordField.text == "admin"){
+			//przejście do nastepnej sceny
+			SceneManager.LoadScene("TestScene");
+		}
 	}
 }
