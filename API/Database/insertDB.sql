4ccdfbb0adbcca9de8eead1037411761348075a1
Oskar <oskarpasko@gmail.com>
2024-05-20 19:57:25 +0200

    pierwsza wersja tabeli "color_test"

--- API/Database/insertDB.sql
+++ API/Database/insertDB.sql
@@ -1,8 +1,9 @@
 /* Dodanie kont podstawowych */
-
 INSERT INTO users VALUES ('oskarpasko@gmail.com', 'Oskar', 'Paśko', '2000-02-12', '609553017', SHA2('oskarpasko2000', 256));
 INSERT INTO users VALUES ('admin@gmail.com', 'Oskar', 'Paśko', '2000-02-12', '111111111', 'admin');
 
 /* Dodanie przykładowego badania */
-
 INSERT INTO pandavision.user_test VALUES(null, CURDATE(), 10, 10, 0, null, 'oskarpasko@gmail.com');
+
+/* Dodanie przykłądowego rekordu do tabeli "color_test" */
+insert into pandavision.color_test values(null, CURDATE(), "153", "czerowny, żółty", "oskarpasko@gmail.com")
