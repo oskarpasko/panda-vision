933a39220c884437bf0ac401a96975f796118c8e
Oskar <oskarpasko@gmail.com>
2024-05-21 12:37:56 +0200

    storzenie bazy kolorów i wpisanie pierwszych 3 testowych kolorów

--- API/Database/insertDB.sql
+++ API/Database/insertDB.sql
@@ -5,5 +5,10 @@ INSERT INTO users VALUES ('admin@gmail.com', 'Oskar', 'Paśko', '2000-02-12', '1
 /* Dodanie przykładowego badania */
 INSERT INTO pandavision.user_test VALUES(null, CURDATE(), 10, 10, 0, null, 'oskarpasko@gmail.com');
 
-/* Dodanie przykłądowego rekordu do tabeli "color_test" */
-insert into pandavision.color_test values(null, CURDATE(), "153", "czerowny, żółty", "oskarpasko@gmail.com")
+/* Dodanie przykłądowego rekordu do tabeli "color_test_user_results" */
+insert into pandavision.color_test_user_results values(null, CURDATE(), "153", "czerowny, żółty", "oskarpasko@gmail.com")
+
+/* Dodanie kolorów do tabelti color_test */
+insert into pandavision.color_test values(null, 255, 0, 0, "Czerwony", "Pomarańczowy", "Niebieski", "Zielony");
+insert into pandavision.color_test values(null, 0, 255, 0, "Zielony", "Pomarańczowy", "Niebieski", "Czerowony");
+insert into pandavision.color_test values(null, 0, 0, 255, "Niebieski", "Pomarańczowy", "Zielony", "Czerowony");
