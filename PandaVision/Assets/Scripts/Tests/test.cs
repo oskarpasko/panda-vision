96dd64426de37f6b7c404c63bd8271970f238890
Oskar <oskarpasko@gmail.com>
2024-09-13 19:19:40 +0200

    check new method

--- PandaVision/Assets/Scripts/Tests/test.cs
+++ PandaVision/Assets/Scripts/Tests/test.cs
@@ -1,31 +1,42 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.Events;
 
 public class test : MonoBehaviour
 {
     [SerializeField] private GameObject button;
     [SerializeField] private int ButtonNumber;
+    [SerializeField] private Button endingButton;
+    [SerializeField] private TMP_Text errorsMessage;
 
     private string[] reds = new string[5];
     private string[] greens = new string[5];
     private string[] blues = new string[5];
+    private int errors = 0;
 
     private void Start() {
         SetCubesNames();
+        endingButton.onClick.AddListener(EndTest);
     }
 
     void OnCollisionStay(Collision other) {
-    //Loop through the buttons array
-    // Check if the colliding object's name matches the name in the reds array
-    if (other.gameObject.name.Equals(reds[ButtonNumber-1])) 
-    {
-        // Change the button's color to blue
-        button.GetComponent<Renderer>().material.color = new Color32(0, 0, 255, 255);
+        // Check if the colliding object's name matches the name in the reds array
+        if (other.gameObject.name != reds[ButtonNumber-1]) errors++;
     }
-}
+
+    void OnCollisionExit(Collision other) {
+        // Check if the colliding object's name matches the name in the reds array
+        if (other.gameObject.name != reds[ButtonNumber-1]) errors--;
+    }
+
+    void EndTest()
+    {
+        errorsMessage.text += errors;
+    }
 
     // method to set names for buttons to check later cubes
     private void SetCubesNames()
