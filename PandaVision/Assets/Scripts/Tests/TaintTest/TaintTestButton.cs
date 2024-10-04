70d47e0b1d144bcbcaf10f8f3f7709d07d851f15
Oskar <oskarpasko@gmail.com>
2024-10-04 22:15:52 +0200

    Update TaintTestButton.cs

--- PandaVision/Assets/Scripts/Tests/TaintTest/TaintTestButton.cs
+++ PandaVision/Assets/Scripts/Tests/TaintTest/TaintTestButton.cs
@@ -5,11 +5,11 @@ public class TaintTestButton : MonoBehaviour
 {
     /// <param name="correctCubeName"> Correct name for specific cube i hierarchy </param>
     /// <param name="error"> Variable which is used count errors </param>
-    [SerializeField] private string correctCubeName;  
+    [SerializeField] private string[] correctCubeName = new string[3];  
     [SerializeField] private Button redButton;
     [SerializeField] private Button greenButton;
     [SerializeField] private Button blueButton;
-    private int whichColor = 0;
+    private string whichColor;
     private bool error = false;
 
     private void Start() {
@@ -23,20 +23,20 @@ public class TaintTestButton : MonoBehaviour
     {
         // if correct cube   -> error = true
         // if uncorrect cube -> error = false
-        error = (collision.gameObject.name != correctCubeName) ? true : false ;
+        error = (collision.gameObject.name != whichColor) ? true : false ;
     }
 
     void OnCollisionExit(Collision collision)
     {
         // Reset the status when the cube leaves the button
-        if (collision.gameObject.name == correctCubeName) error = false;
+        if (collision.gameObject.name == whichColor) error = false;
     }
 
     // Method to check if the correct cube is on the button
     public bool Error() { return error; }
-    public void setColorToRed() { whichColor = 0; }
-    public void setColorToGreen() { whichColor = 1; }
-    public void setColorToBlue() { whichColor = 2; }
+    public void setColorToRed() { whichColor = correctCubeName[0]; }
+    public void setColorToGreen() { whichColor = correctCubeName[1]; }
+    public void setColorToBlue() { whichColor = correctCubeName[2]; }
     public int getWhichColor() { return whichColor; }
     
 }
