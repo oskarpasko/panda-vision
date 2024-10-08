254b398e189b8e64e6fdbfe3ca1f003135465695
Oskar <oskarpasko@gmail.com>
2024-10-08 10:06:08 +0200

    Update TaintTestButton.cs

--- PandaVision/Assets/Scripts/Tests/TaintTest/TaintTestButton.cs
+++ PandaVision/Assets/Scripts/Tests/TaintTest/TaintTestButton.cs
@@ -5,10 +5,8 @@ using UnityEngine.UI;
 public class TaintTestButton : MonoBehaviour
 {
     /// <param name="correctCubeName"> Correct name for specific cube i hierarchy </param>
-    /// <param name="error"> Variable which is used count errors </param>
     /// <param name="whichColor"> Variavble with correct color's names </param>
-    /// <param name="error"> Variable to check errors </param>
-    /// <param name="getColor"> Instance of GetColor class to get color later in the code </param>
+    /// <param name="error"> Variable which is used count errors </param>
     [SerializeField] private string[] correctCubeName = new string[3];  
     private string whichColor;
     private bool error = false;
