3544d1f6c0aa99f6dc622088227c5a5f58498e1d
Oskar <oskarpasko@gmail.com>
2024-10-20 09:47:47 +0200

    add admin page for main page and update other files

--- API/Scripts/views/api/db_config.py
+++ API/Scripts/views/api/db_config.py
@@ -5,7 +5,7 @@ def get_db_connection():
     return pymysql.connect(
         host='localhost',        # Database host
         user='root',             # Database username
-        password='admin',   # Database password
+        password='admin',        # Database password
         db='pandavision',        # Database name
         cursorclass=pymysql.cursors.DictCursor
     )
