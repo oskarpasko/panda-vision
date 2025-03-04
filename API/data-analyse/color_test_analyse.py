db7d3043c8f267a703784a240e222325015a9bd7
Oskar <oskarpasko@gmail.com>
2025-03-04 11:20:49 +0100

    Create color_test_analyse.py

---
+++ API/data-analyse/color_test_analyse.py
@@ -0,0 +1,26 @@
+import pymysql
+from plot_2d import plot_2d
+
+# connect with db
+db = pymysql.connect(
+    host="localhost",    
+    user="root",  
+    password="oskarpasko",   
+    database="pandavision"  
+)
+cursor = db.cursor()
+
+# query
+sql = """
+SELECT time_of_test, error_colors, sex
+FROM pandavision.color_test_user_results
+LEFT JOIN pandavision.users ON color_test_user_results.user = users.username;
+"""
+
+cursor.execute(sql)
+data = cursor.fetchall()
+cursor.close()
+db.close()
+
+# plot
+plot_2d(data, 'Wykres wyników Tesu Kolorów')
