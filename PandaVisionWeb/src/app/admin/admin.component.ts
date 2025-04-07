908b49d28a19bab0ff627eb3cc37628f2b19e96e
Oskar <oskarpasko@gmail.com>
2025-04-07 11:48:19 +0200

    add some table for data from tests

--- PandaVisionWeb/src/app/admin/admin.component.ts
+++ PandaVisionWeb/src/app/admin/admin.component.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { API_CONFIG } from '../api-endpoints';
 
 @Component({
   standalone: false,
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.scss']
 })
-export class AdminComponent {
-  username: string = JSON.parse(localStorage.getItem('user') || '{}').username;
+export class AdminComponent implements OnInit {
+  colorTests: any[] = [];
+  taintTests: any[] = [];
+  ishiharaTests: any[] = [];
+  twoColorsTests: any[] = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get<any>(API_CONFIG.baseUrl + '/api/admin_mongo').subscribe(data => {
+      this.colorTests = data.color_tests;
+      this.taintTests = data.taint_tests;
+      this.ishiharaTests = data.ishihara_tests;
+      this.twoColorsTests = data.two_colors_tests;
+    });
+  }
 }
